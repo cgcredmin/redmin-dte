e9f6e1baa959fd0ca83c757e24f04b2a4f2680dd
CristobalMontecinos <cristobal.montecinos04@gmail.com>
2024-02-16 11:39:17 -0300

    Migracione de sp creadas

---
+++ src/database/migrations/2024_02_16_111859_create_sp_process_taxpayers_data.php
@@ -0,0 +1,120 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        return <<<SQL
+            CREATE PROCEDURE `processTaxpayersData` (IN `p_limit` int)
+            BEGIN
+
+                #Cursor variables
+                DECLARE c_rut 	 		   varchar(20);
+                DECLARE c_razon_social 	   varchar(255);
+                DECLARE c_nro_resolucion   varchar(10);
+                DECLARE c_fecha_resolucion varchar(20);
+                DECLARE c_correo 		   varchar(100);
+
+                #Otras variables 
+                DECLARE rut_numeros		   int;
+                DECLARE digito_verificador varchar(1);
+                DECLARE contador           int DEFAULT 0;
+
+                DROP TABLE IF EXISTS temp_bulk_copy;
+                CREATE TEMPORARY TABLE IF NOT EXISTS temp_bulk_copy (
+                    rut              varchar(20),
+                    razon_social     varchar(255),
+                    nro_resolucion   varchar(50),
+                    fecha_resolucion varchar(20),
+                    correo           varchar(100)
+                );
+
+                #Construyo cursor
+                BEGIN
+                    DECLARE taxpayersCursor CURSOR FOR
+                        SELECT BC.field_1 AS RUT
+                            , BC.field_2 AS RAZON_SOCIAL
+                            , BC.field_3 AS NRO_RESOLUCION
+                            , BC.field_4 AS FECHA_RESOLUCION
+                            , BC.field_5 AS CORREO 
+                        FROM bulkCopyTable AS BC
+                        WHERE field_1 REGEXP '^[0-9]{6,8}-[0-9kK]$'
+                        AND BC.field_3 IS NOT NULL
+                        AND BC.field_4 IS NOT NULL
+                        AND BC.field_5 IS NOT NULL
+                        AND BC.field_3 REGEXP '^[0-9]+$';
+
+                    OPEN taxpayersCursor;
+                        BEGIN
+                            DECLARE fin_l bool default FALSE;
+                            DECLARE continue HANDLER FOR NOT FOUND SET fin_l = TRUE; 
+
+                            loop_taxprayers: LOOP
+                                FETCH taxpayersCursor INTO c_rut, c_razon_social, c_nro_resolucion, c_fecha_resolucion, c_correo;
+                                    IF fin_l THEN
+                                        LEAVE loop_taxprayers;
+                                    END IF;
+
+                                    #Aqui se separa el rut en dos partes
+                                    SET rut_numeros = CAST(SUBSTRING_INDEX(c_rut, '-', 1) AS UNSIGNED);
+                                    SET digito_verificador = SUBSTRING_INDEX(c_rut, '-', -1);
+
+                                    #Acá reemplazo posibles valores que induzcan problemas
+                                    SET c_razon_social = REPLACE(c_razon_social, "'", "''");
+                                    SET c_razon_social = REPLACE(c_razon_social, '"', '""');
+
+                                    INSERT INTO temp_bulk_copy (rut, razon_social, nro_resolucion, fecha_resolucion, correo)
+                                    VALUES (c_rut, c_razon_social, c_nro_resolucion, c_fecha_resolucion, c_correo);
+
+                                    SET contador = contador + 1;
+
+                                    IF contador >= 50000 THEN
+                                        #Insertar todas las filas acumuladas en la tabla final
+                                        INSERT INTO contribuyentes (rut, dv, razon_social, nro_resolucion, fecha_resolucion, correo)
+                                        SELECT rut_numeros, digito_verificador, razon_social, nro_resolucion, fecha_resolucion, correo
+                                        FROM temp_bulk_copy;
+
+                                        #Limpiar la tabla temporal
+                                        DELETE FROM temp_bulk_copy;
+
+                                        #Restablecer el contador de filas
+                                        SET contador = 0;
+                                    END IF;
+
+                            END LOOP loop_taxprayers;
+
+                            IF contador > 0 THEN
+                                INSERT INTO contribuyentes (rut, dv, razon_social, nro_resolucion, fecha_resolucion, correo)
+                                SELECT rut_numeros, digito_verificador, razon_social, nro_resolucion, fecha_resolucion, correo
+                                FROM temp_bulk_copy;
+                            END IF;
+
+                        END;
+                    CLOSE taxpayersCursor;
+                END;
+
+            END;
+        SQL;
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        return <<<SQL
+            DROP PROCEDURE IF EXISTS `processTaxpayersData`;
+        SQL;
+    }
+};
---
+++ src/database/migrations/2024_02_16_111926_create_sp_get_taxpayers_data.php
@@ -0,0 +1,192 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        return <<<SQL
+            CREATE PROCEDURE `getTaxpayersData` (IN `p_limit` int)
+            BEGIN
+
+                #Cursor variables
+                DECLARE c_field_1 varchar(255);
+                DECLARE c_field_2 varchar(255);
+                DECLARE c_field_3 varchar(255);
+                DECLARE c_field_4 varchar(255);
+                DECLARE c_field_5 varchar(255);
+                DECLARE c_field_6 varchar(255);
+                DECLARE c_field_7 varchar(255);
+                DECLARE c_field_8 varchar(255);
+                DECLARE c_field_9 varchar(255);
+                DECLARE c_field_10 varchar(255);
+                DECLARE c_id      int;
+
+                #Otras variables 
+                DECLARE rut 		 varchar(20);
+                DECLARE rutFound     BOOL DEFAULT FALSE; 
+                DECLARE email	     varchar(100);
+                DECLARE emailFound   BOOL DEFAULT FALSE;
+
+                #Expresiones regulares
+                DECLARE rutRegExp 	varchar(50)  DEFAULT '^[0-9]{6,8}-[0-9kK]$';
+                DECLARE emailRegExp varchar(100) DEFAULT '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$';
+
+                DROP TABLE IF EXISTS temp_wrong_data;
+                CREATE TEMPORARY TABLE IF NOT EXISTS temp_wrong_data AS (
+                    SELECT BC.field_1 AS field_1
+                        , BC.field_2 AS field_2
+                        , BC.field_3 AS field_3
+                        , BC.field_4 AS field_4
+                        , BC.field_5 AS field_5
+                        , BC.field_6 AS field_6
+                        , BC.field_7 AS field_7
+                        , BC.field_8 AS field_8
+                        , BC.field_9 AS field_9
+                        , BC.field_10 AS field_10
+                        , id
+                    FROM bulkCopyTable AS BC
+                    WHERE field_1 NOT REGEXP rutRegExp
+                        OR field_3 IS NULL 
+                        OR field_4 IS NULL 
+                        OR field_5 IS NULL
+                        OR field_3 NOT REGEXP '^[0-9]+$'
+                );
+
+                #Construyo cursor
+                BEGIN
+                    DECLARE taxpayersCursor CURSOR FOR
+                        SELECT WD.field_1 
+                            , WD.field_2 
+                            , WD.field_3 
+                            , WD.field_4 
+                            , WD.field_5 
+                            , WD.field_6
+                            , WD.field_7
+                            , WD.field_8
+                            , WD.field_9
+                            , WD.field_10
+                            , id
+                        FROM temp_wrong_data AS WD
+                        WHERE WD.field_2 IS NOT NULL;
+
+                    OPEN taxpayersCursor;
+                        BEGIN
+                            DECLARE fin_l bool default FALSE;
+                            DECLARE continue HANDLER FOR NOT FOUND SET fin_l = TRUE; 
+
+                            loop_taxprayers: LOOP
+                                FETCH taxpayersCursor INTO c_field_1, c_field_2, c_field_3, c_field_4, c_field_5
+                                                        , c_field_6, c_field_7, c_field_8, c_field_9, c_field_10, c_id;
+                                    IF fin_l THEN
+                                        LEAVE loop_taxprayers;
+                                    END IF;
+
+                                    #Busco rut del contribuyente
+                                    IF c_field_1 REGEXP rutRegExp THEN
+                                        SET rut = c_field_1;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_2 REGEXP rutRegExp THEN
+                                        SET rut = c_field_2;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_3 REGEXP rutRegExp THEN
+                                        SET rut = c_field_3;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_4 REGEXP rutRegExp THEN
+                                        SET rut = c_field_4;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_5 REGEXP rutRegExp THEN
+                                        SET rut = c_field_5;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_6 REGEXP rutRegExp THEN
+                                        SET rut = c_field_6;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_7 REGEXP rutRegExp THEN
+                                        SET rut = c_field_7;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_8 REGEXP rutRegExp THEN
+                                        SET rut = c_field_8;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_9 REGEXP rutRegExp THEN
+                                        SET rut = c_field_9;
+                                        SET rutFound = TRUE;
+                                    ELSEIF c_field_10 REGEXP rutRegExp THEN
+                                        SET rut = c_field_10;
+                                        SET rutFound = TRUE;
+                                    END IF;
+
+                                    #Busco correo del contribuyente
+                                    IF c_field_1 REGEXP emailRegExp THEN
+                                        SET email = c_field_1;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_2 REGEXP emailRegExp THEN
+                                        SET email = c_field_2;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_3 REGEXP emailRegExp THEN
+                                        SET email = c_field_3;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_4 REGEXP emailRegExp THEN
+                                        SET email = c_field_4;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_5 REGEXP emailRegExp THEN
+                                        SET email = c_field_5;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_6 REGEXP emailRegExp THEN
+                                        SET email = c_field_6;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_7 REGEXP emailRegExp THEN
+                                        SET email = c_field_7;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_8 REGEXP emailRegExp THEN
+                                        SET email = c_field_8;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_9 REGEXP emailRegExp THEN
+                                        SET email = c_field_9;
+                                        SET emailFound = TRUE;
+                                    ELSEIF c_field_10 REGEXP emailRegExp THEN
+                                        SET email = c_field_10;
+                                        SET emailFound = TRUE;
+                                    END IF;
+
+                                    IF rutFound = TRUE AND emailFound = TRUE THEN
+                                        INSERT INTO contribuyentes_erroneos (rut, correo)
+                                        VALUES (rut, email);
+
+                                        SET rut = '';
+                                        SET email = '';
+                                        SET rutFound = FALSE;
+                                        SET emailFound = FALSE;
+
+                                    END IF;
+
+                            END LOOP loop_taxprayers;
+                        END;
+                    CLOSE taxpayersCursor;
+                END;
+
+                SELECT 'PROCESO FINALIZADO CORRECTAMENTE';
+
+            END;
+        SQL;
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        return <<<SQL
+            DROP PROCEDURE IF EXISTS `getTaxpayersData`;
+        SQL;
+    }
+};
