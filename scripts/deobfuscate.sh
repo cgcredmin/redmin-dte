5048258bb8bf1d9e7fca3bcf8b131997d01dcbf6
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-02-23 11:28:11 -0300

    Remove MySQL and obfuscation scripts

--- scripts/deobfuscate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_PATH="$PWD"
-
-rm -rf $PROJECT_PATH/src/app/Http/Controllers
-  
-if [[ -d "backup/Controllers" ]]; then
-  cp -a $PROJECT_PATH/backup/Controllers/. $PROJECT_PATH/src/app/Http/Controllers/
-fi
