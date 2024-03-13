901c5ea2592522fc4f064adc1709c42d5a1ae366
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-03-13 10:56:14 -0300

    :fix permission block on init.py

--- src/init.py
+++ src/init.py
@@ -10,7 +10,8 @@ base_path = "/var/www/html/"
 os.chdir(base_path)
 
 folders = ["storage/app", "storage/app/certs", "storage/app/pdf", "storage/app/db", "storage/app/tempfiles", "storage/app/tmp", "storage/app/xml", "storage/app/xml/dte",
-           "storage/app/xml/folios", "storage/framework", "storage/cache", "storage/cache/data", "storage/sessions", "storage/testing", "storage/views", "storage/logs", "tests"]
+           "storage/app/xml/folios", "storage/framework", "storage/framework/views", "storage/framework/cache", "storage/framework/cache/data",
+           "storage/cache", "storage/cache/data", "storage/sessions", "storage/testing", "storage/views", "storage/logs", "tests"]
 # loop through the array and check if the folder exists, if not, then create it and set permissions
 print(":eyes:", ">> [blue]Checking folders...[/blue]")
 for folder in folders:
@@ -18,6 +19,8 @@ for folder in folders:
         os.makedirs(folder)
         print(":white_check_mark:",
               "\t - [green]" + folder + " CREATED.[/green]")
+
+    print(":key:", "\t - [blue]Setting permissions for " + folder + " ...[/blue]")
     os.chmod(folder, 0o777)
     os.chown(folder, 1000, 1000)
 
